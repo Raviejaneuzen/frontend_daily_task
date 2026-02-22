c7c6a9a4b528b237dc00db26c86f7fb89d78ced5
Raviejaneuzen <raviteja.u@neuzenai.com>
2026-02-22 20:36:35 +0530

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://daily-repot.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'https://backend-daily-task.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
